dc50a452a43e6d1c2744f773aaa2e14c1f7741bd
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-03 16:37:28 -0300

    feat: 🎸 first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Image
+        src="/logo.jpg"
+        alt="Rafaela Sambiasse"
+        className="dark:invert"
+        width={1080}
+        height={600}
+        priority
+        />
+        <p className="text-xl">Em contrução... 😊</p>
+    </main>
+  );
+}
