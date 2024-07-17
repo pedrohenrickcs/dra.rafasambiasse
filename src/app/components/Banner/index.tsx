655fce9401324a9e7d72830bf582670b11d8b484
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-17 14:16:10 -0300

    feat: 🎸 info

--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -14,18 +14,6 @@ export const Banner = () => {
           </div>
         </div>
       </div>
-
-      <div className="container text-left m-auto flex md:hidden flex-col justify-center md:p-6">
-        <div className="md:w-1/2 text-black-text-color">
-          <h1 className="md:text-5xl font-bold">
-            Seu sorriso é a nossa maior prioridade
-          </h1>
-          <h3 className="mt-4">
-            Valorizamos cada paciente e buscamos sempre oferecer um serviço de
-            alta qualidade, garantindo conforto e satisfação em cada visita.
-          </h3>
-        </div>
-      </div>
     </>
   )
 }
--- src/app/components/ContentInfo/index.tsx
+++ src/app/components/ContentInfo/index.tsx
@@ -1,39 +1,49 @@
-import { FaRegHeart } from 'react-icons/fa'
+import { FaAward, FaRegHeart, FaTooth } from 'react-icons/fa'
 
 export const ContentInfo = () => {
   return (
-    <div className="container flex text-center items-center bg-primary-bg-color text-primary-text-color p-10 -my-10">
-      <div className="px-10 h-40 flex flex-col justify-center border-r">
+    <div className="container flex text-center flex-col xl:flex-row items-center bg-primary-bg-color text-primary-text-color p-10 md:-my-10">
+      <div className="md:w-1/2 text-primary-text-color md:text-black-text-color md:hidden">
+        <h1 className="text-2xl md:text-5xl font-bold">
+          Seu sorriso é a nossa maior prioridade
+        </h1>
+      </div>
+      <div className="px-5 md:px-10 mt-8 md:h-40 flex flex-col justify-center xl:border-r">
         <span className="flex justify-center pb-4">
-          <FaRegHeart size={40} />
+          <FaAward size={40} />
         </span>
 
-        <h2 className="md:text-xl">Profissionais Experientes</h2>
-        <p className="text-sm mt-4">
-          Temos profissionais experientes, com excelente formação e que atuam há
-          muitos anos no mercado.
+        <h2 className="text-xl font-medium">
+          Excelência em Serviços Odontológicos
+        </h2>
+        <p className="text-base mt-4">
+          Nos orgulhamos de oferecer serviços odontológicos de excelência,
+          utilizando materiais de qualidade para garantir os melhores resultados
+          e a satisfação de nossos pacientes.
         </p>
       </div>
-      <div className="px-10 h-40 flex flex-col justify-center border-r">
+      <div className="px-5 md:px-10 mt-8 md:h-40 flex flex-col justify-center xl:border-r">
         <span className="flex justify-center pb-4">
-          <FaRegHeart size={40} />
+          <FaTooth size={40} />
         </span>
 
-        <h2 className="md:text-xl">Profissionais Experientes</h2>
-        <p className="text-sm mt-4">
-          Temos profissionais experientes, com excelente formação e que atuam há
-          muitos anos no mercado.
+        <h2 className="text-xl font-medium">Atendimento Humanizado</h2>
+        <p className="text-base mt-4">
+          Cada tratamento é planejado de acordo com as necessidades específicas
+          de cada paciente, oferecendo soluções personalizadas que garantem
+          resultados estéticos e funcionais excepcionais.
         </p>
       </div>
-      <div className="px-10 h-40 flex flex-col justify-center">
+      <div className="px-5 md:px-10 mt-8 md:h-40 flex flex-col justify-center">
         <span className="flex justify-center pb-4">
           <FaRegHeart size={40} />
         </span>
 
-        <h2 className="md:text-xl">Profissionais Experientes</h2>
-        <p className="text-sm mt-4">
-          Temos profissionais experientes, com excelente formação e que atuam há
-          muitos anos no mercado.
+        <h2 className="text-xl font-medium">Atendimento Personalizado</h2>
+        <p className="text-base mt-4">
+          Cada tratamento é planejado de acordo com as necessidades específicas
+          de cada paciente, oferecendo soluções personalizadas que garantem
+          resultados estéticos e funcionais excepcionais.
         </p>
       </div>
     </div>
