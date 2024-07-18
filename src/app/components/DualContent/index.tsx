da9f6f9780e2bfb68a9fae41410620fe7f757a2d
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-18 15:49:49 -0300

    feat: 🎸 adjust text

--- src/app/components/DualContent/index.tsx
+++ src/app/components/DualContent/index.tsx
@@ -5,12 +5,18 @@ const DualContent = () => {
   return (
     <div className="px-6 py-12 text-center flex flex-col md:flex-row items-center justify-center md:justify-around text-black-text-color md:text-left">
       <div className="w-full md:w-1/2">
-        <h2 className="text-4xl font-bold">Sobre mim</h2>
+        <h2 className="text-4xl font-bold">Muito Bem-vindos!</h2>
         <p className="py-4">
-          Olá! Sou a Dra. Rafaela, dentista com anos de experiência dedicado a
-          cuidar da sua saúde bucal com atenção, profissionalismo e empatia.
-          Estou aqui para transformar seu sorriso, proporcionando tratamentos
+          Sou a Dra. Rafaela, dentista com anos de experiência dedicado a cuidar
+          da sua saúde bucal com atenção, profissionalismo e empatia. Estou aqui
+          para transformar seu sorriso, proporcionando tratamentos
           personalizados que atendem às suas necessidades e expectativas.
+          <br />
+          <br />
+          Acredito que cada paciente merece um atendimento individualizado e
+          humanizado. Meu objetivo é não apenas tratar problemas dentários, mas
+          também promover a saúde bucal de forma integral, educando e prevenindo
+          para garantir sorrisos saudáveis e duradouros.
         </p>
         <div className="hidden md:flex">
           <Button type="primary" />
