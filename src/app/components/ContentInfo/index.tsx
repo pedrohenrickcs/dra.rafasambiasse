d9f7b693bb62ba213d2a7142944115b224edddd6
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-17 21:55:57 -0300

    feat: 🎸 adjust text

--- src/app/components/ContentInfo/index.tsx
+++ src/app/components/ContentInfo/index.tsx
@@ -8,7 +8,7 @@ export const ContentInfo = () => {
           Seu sorriso é a nossa maior prioridade
         </h1>
       </div>
-      <div className="md:px-10 mt-8 md:h-40 flex flex-col justify-center xl:border-r">
+      <div className="md:px-10 my-8 md:h-40 flex flex-col justify-center xl:border-r">
         <span className="flex justify-center pb-4">
           <FaAward size={40} />
         </span>
@@ -16,31 +16,31 @@ export const ContentInfo = () => {
         <h2 className="text-xl font-medium">
           Excelência em Serviços Odontológicos
         </h2>
-        <p className="text-base mt-4">
+        <p className="text-base my-4">
           Nos orgulhamos de oferecer serviços odontológicos de excelência,
           utilizando materiais de qualidade para garantir os melhores resultados
           e a satisfação de nossos pacientes.
         </p>
       </div>
-      <div className="md:px-10 mt-8 md:h-40 flex flex-col justify-center xl:border-r">
+      <div className="md:px-10 my-8 md:h-40 flex flex-col justify-center xl:border-r">
         <span className="flex justify-center pb-4">
           <FaRegHeart size={40} />
         </span>
 
         <h2 className="text-xl font-medium">Atendimento Humanizado</h2>
-        <p className="text-base mt-4">
+        <p className="text-base my-4">
           Cada paciente é único. Adaptamos nosso atendimento para atender às
           suas necessidades e preferências específicas, garantindo que você se
           sinta confortável e seguro durante todo o processo.
         </p>
       </div>
-      <div className="md:px-10 mt-8 md:h-40 flex flex-col justify-center">
+      <div className="md:px-10 my-8 md:h-40 flex flex-col justify-center">
         <span className="flex justify-center pb-4">
           <FaTooth size={40} />
         </span>
 
         <h2 className="text-xl font-medium">Atendimento Personalizado</h2>
-        <p className="text-base mt-4">
+        <p className="text-base my-4">
           Cada tratamento é planejado de acordo com as necessidades específicas
           de cada paciente, oferecendo soluções personalizadas que garantem
           resultados estéticos e funcionais excepcionais.
