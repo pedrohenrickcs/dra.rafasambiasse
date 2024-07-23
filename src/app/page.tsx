46953171d6d294b6d48537bf61752179dda2836a
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-23 13:22:27 -0300

    feat: 🎸 lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { items } from './components/Banner/mock'
 import { infos } from './components/ContentInfo/mock'
 import { infosContent } from './components/DualContent/mock'
-import { infosTextContent, infosBeforeAfter } from './components/TextContent/mock'
+import { 
+  infosTextContent, 
+  infosBeforeAfter 
+} from './components/TextContent/mock'
 
 import { Banner } from './components/Banner'
 import { ContentInfo } from './components/ContentInfo'
